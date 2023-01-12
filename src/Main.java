fb6e20879882527cc50d1b89ff5daed4a34e1067
DavidP1996erea <david.perea@iesnervion.es>
2023-01-12 11:12:57 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+                try {
+                    // 1 - Crear un socket de tipo cliente indicando IP y puerto del servidor
+                    System.out.println("Estableciendo conexión con el servidor");
+                    Socket cliente = new Socket("192.168.1.137", 49200);
+
+                    // 2 - Abrir flujos de lectura y escritura
+                    InputStream is = cliente.getInputStream();
+                    OutputStream os = cliente.getOutputStream();
+
+                    // 3 - Intercambiar datos con el servidor
+                    // Envío de mensaje de texto al servidor
+                    OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+                    BufferedWriter bw = new BufferedWriter(osw);
+                    bw.write("Soy el cliente. Envío este mensaje al servidor");
+                    bw.newLine();
+                    bw.flush();
+
+                    // Leo mensajes que me envía el servidor
+                    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                    BufferedReader br = new BufferedReader(isr);
+                    System.out.println("El servidor me envía el siguiente mensaje: " + br.readLine());
+
+                    // 4 - Cerrar flujos de lectura y escritura
+                    br.close();
+                    isr.close();
+                    is.close();
+                    bw.close();
+                    osw.close();
+                    os.close();
+
+                    // 5 - Cerrar la conexión
+                    System.out.println("Se cierra la conexión del cliente");
+                    cliente.close();
+
+                } catch (UnknownHostException e) {
+                    System.err.println("No se encuentra el host especificado.");
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    System.err.println("Se ha producido un error en la conexión con el servidor.");
+                    e.printStackTrace();
+                }
+            }
+ }
---
+++ src/Server.java
@@ -0,0 +1,60 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+    public class Server {
+        public static void main(String[] args) {
+
+            try {
+                // 1 - Crear socket de tipo servidor y le indicamos el puerto
+                ServerSocket servidor = new ServerSocket(36000);
+
+                // 2 - Queda a la espera de peticiones y las acepta cuando las recibe
+                System.out.println("Servidor se encuentra a la escucha...");
+                Socket peticion = servidor.accept();
+
+                // 3 - Abrir flujos de lectura y escritura de datos
+                InputStream is = peticion.getInputStream();
+                OutputStream os = peticion.getOutputStream();
+
+                // 4 - Intercambiar datos con el cliente
+                // Leer mensaje enviado por el cliente e imprimirlo por consola
+                InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                System.out.println("Mensaje enviado por el cliente: " + br.readLine());
+
+                // Enviarle mensaje al cliente
+                System.out.println("Servidor envía al cliente un mensaje");
+                OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+                BufferedWriter bw = new BufferedWriter(osw);
+                bw.write("Soy el servidor. Este mensaje es para el cliente");
+                bw.newLine();
+                bw.flush();
+
+                // 5 - Cerrar flujos de lectura y escritura
+                br.close();
+                isr.close();
+                is.close();
+                bw.close();
+                osw.close();
+                os.close();
+
+                // 6 - Cerra la conexión
+                System.out.println("Cierre de conexión del servidor");
+                peticion.close();
+                servidor.close();
+
+            } catch (IOException e) {
+                System.err.println("Ha habido algún error en la creación del Socket Servidor");
+                e.printStackTrace();
+            }
+        }
+}
